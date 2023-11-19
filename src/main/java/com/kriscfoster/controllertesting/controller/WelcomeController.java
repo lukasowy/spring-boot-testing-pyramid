1dea777f18369bfebdca5a44d64ef647ca1ccfe1
Lukasz <lukasowy@gmail.com>
2023-11-19 18:19:56 +0100

    Add a constructor

--- src/main/java/com/kriscfoster/controllertesting/controller/WelcomeController.java
+++ src/main/java/com/kriscfoster/controllertesting/controller/WelcomeController.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
 public class WelcomeController {
 
     private final WelcomeService welcomeService;
+
+    public WelcomeController(WelcomeService welcomeService) {
+        this.welcomeService = welcomeService;
+    }
 
     @GetMapping("/welcome")
     public String welcome(@RequestParam(defaultValue = "Stranger") String name) {
